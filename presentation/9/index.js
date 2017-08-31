49f7afd710ad8870f13c65ac15076f4e7393aacb
Anthony Tarr <Anthony.Tarr@cba.com.au>
2017-08-31 11:49:44 +1000

    added franc

--- presentation/9/index.js
+++ presentation/9/index.js
@@ -23,4 +23,5 @@ export const Links =
     <Heading size={4} margin="0 0 1em 0">Useful Links</Heading>
     <Text>http://blog.stevensanderson.com/2016/05/02/angular2-react-knockout-apps-on-aspnet-core/</Text>
     <Text>https://blogs.msdn.microsoft.com/dotnet/2017/08/14/announcing-net-core-2-0/</Text>
+    <Text>https://github.com/wooorm/franc</Text>
   </Slide>;
